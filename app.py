ccd61f8dc5be9efa4270ed2fc739e7e4007fd5d4
Artur F. da Rocha <arthurrocha_@hotmail.com>
2019-06-13 04:37:22 +0000

    adding comments

--- app.py
+++ app.py
@@ -1,33 +1,40 @@
 import subprocess
 import logging
+import time
+import datetime
 
-logging.basicConfig(filename='data.csv', level=logging.INFO)
+logging.basicConfig(format='%(message)s', filename='data.csv', level=logging.INFO)
 
 
 def shell_cmd(cmd):
-	result = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	output = result.communicate()[0]
-	return output
+    result = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = result.communicate()[0]
+    return output
 
 
-url = "http://www.dublincity.ie/dublintraffic/cpdata.xml"
+cmd = "curl http://www.dublincity.ie/dublintraffic/cpdata.xml"
 
-cmd = "curl {0}".format(url)
+while True:
+    # get time 
+    # format = 2017-03-06 16:00:04.159338
+    # + timedelta because time is in UTC +0
+    date_now = str(datetime.datetime.now() + datetime.timedelta(seconds=60*60))
+    
+    # get the xml as list and split by lines
+    result = shell_cmd(cmd).decode('utf-8').split("\n")
+    
+    # get the current timestamp on xml
+    for thing in result:
+        if "Timestamp" in thing:
+            timestamp = thing.split("<Timestamp>")[1].split(" ")[0]
 
-
-result = shell_cmd(cmd).decode('utf-8').split("\n")
-#print(result)
-#result = result[0].split("\n")
-
-
-for thing in result:
-	if "Timestamp" in thing:
-		timestamp = thing.split("<Timestamp>")[1].split(" ")[0]
-
-
-for thing in result:
-	if "carpark name=" in thing:
-		var = thing.split("\"")
-		print(var)		
-		data_to_log = ',{0},{1},{2}'.format(str(var[1]), str(var[3]), str(timestamp))
-		logging.info(data_to_log)
+    # get the results and append todata.csv
+    # format = PARKING NAME, VALUE, XML TIMESTAMP, CURRENT DATETIME 
+    for thing in result:
+        if "carpark name=" in thing:
+            var = thing.split("\"")
+            data_to_log = '{0},{1},{2},{3}'.format(str(var[1]), str(var[3]), str(timestamp), date_now)
+            logging.info(data_to_log)
+    
+    # wait 5 min and run again
+    time.sleep(60*5)
